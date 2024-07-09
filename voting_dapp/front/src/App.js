5953b48e813c96eb5ba65fb6e5d60d55d3a0e437
Nooorrrr <noor.bnhdd@gmail.com>
2024-07-09 09:07:24 +0200

    added metamask login

--- voting_dapp/front/src/App.js
+++ voting_dapp/front/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Login from './components/login';
+import { useState, useEffect } from 'react';
+import { contractAdress, contractABI } from './consts/contract';
+import { ethers } from 'ethers';
 
 function App() {
   return (
     <div className="App">
       <h1>welcom to our lovely very beuatifull voting dapp</h1>
+      <Login />
     </div>
   );
 }
