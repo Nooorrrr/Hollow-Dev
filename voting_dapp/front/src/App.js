718525dfbf8ac1bbbc0f937c50aa7dec08e4bf0e
Nooorrrr <noor.bnhdd@gmail.com>
2024-07-09 09:29:00 +0200

    meta mask auth

--- voting_dapp/front/src/App.js
+++ voting_dapp/front/src/App.js
@@ -6,10 +6,37 @@ import { contractAdress, contractABI } from './consts/contract';
 import { ethers } from 'ethers';
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+
+ 
+
+  async function MetamaskAuth() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected : " + address);
+        setIsConnected(true);
+        
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.error("Metamask is not detected in the browser")
+    }
+  }
+
   return (
     <div className="App">
       <h1>welcom to our lovely very beuatifull voting dapp</h1>
-      <Login />
+      <Login connectWallet ={MetamaskAuth} />
     </div>
   );
 }
